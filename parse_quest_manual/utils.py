34d7f4a9b88acad112d0ace684c3cd4d38acb4a9
Steve Casey <stevencasey21@gmail.com>
2019-04-05 00:31:02 -0400

    moved null value to global constants file

--- parse_quest_manual/utils.py
+++ parse_quest_manual/utils.py
@@ -1,8 +1,7 @@
 import re
-from parse_quest_manual import _constants_items as c
+from ._constants_items import *
+from ._constants_global import NULL_VALUE
 from typing import Union, Dict
-
-NULL_VALUE = '——'
 
 
 def map_key(key: str) -> str:
@@ -31,26 +30,26 @@ def parse_traits(traits):
 def map_value(key: str, value: str) -> Union[int, str]:
     key = key.lower()
     switcher = {
-        c.DT: lambda: str_to_int(value),
-        c.VALUE: lambda: str_to_int(value),
-        c.WEIGHT: lambda: str_to_int(value),
-        c.ACCURACY: lambda: str_to_int(value),
-        c.DEFENSE: lambda: str_to_int(value),
-        c.MAGAZINE: lambda: str_to_int(value),
-        c.SMAG: lambda: str_to_int(value),
-        c.MMAG: lambda: str_to_int(value),
-        c.LMAG: lambda: str_to_int(value),
-        c.RADIATION: lambda: str_to_int(value),
-        c.STRENGTH: lambda: str_to_int(value),
-        c.TRAITS: lambda: parse_traits(value),
-        c.DAMAGE: lambda: dice_to_list(value),
-        c.EFFECT: lambda: parse_traits(value),
-        c.VALUEMOD: lambda: str_to_int(value),
-        c.DURATION: lambda: str_to_int(value),
-        c.ADDICTIONSAVE: lambda: str_to_int(value),
-        c.HP: lambda: dice_to_list(value),
-        c.RADIATION: lambda: str_to_int(value),
-        c.COMPONENTIN: lambda: parse_traits(value)
+        DT: lambda: str_to_int(value),
+        VALUE: lambda: str_to_int(value),
+        WEIGHT: lambda: str_to_int(value),
+        ACCURACY: lambda: str_to_int(value),
+        DEFENSE: lambda: str_to_int(value),
+        MAGAZINE: lambda: str_to_int(value),
+        SMAG: lambda: str_to_int(value),
+        MMAG: lambda: str_to_int(value),
+        LMAG: lambda: str_to_int(value),
+        RADIATION: lambda: str_to_int(value),
+        STRENGTH: lambda: str_to_int(value),
+        TRAITS: lambda: parse_traits(value),
+        DAMAGE: lambda: dice_to_list(value),
+        EFFECT: lambda: parse_traits(value),
+        VALUEMOD: lambda: str_to_int(value),
+        DURATION: lambda: str_to_int(value),
+        ADDICTIONSAVE: lambda: str_to_int(value),
+        HP: lambda: dice_to_list(value),
+        RADIATION: lambda: str_to_int(value),
+        COMPONENTIN: lambda: parse_traits(value)
     }
 
     return switcher.get(key, value.lower)()
